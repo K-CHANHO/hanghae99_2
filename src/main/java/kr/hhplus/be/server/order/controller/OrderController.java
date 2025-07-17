1d4b5352b7a3baa7021e199a7dd857260e51ad27
chanos <cksgh1565@gmail.com>
2025-07-18 01:29:30 +0900

    [test] 주문 API

---
+++ src/main/java/kr/hhplus/be/server/order/controller/OrderController.java
@@ -0,0 +1,34 @@
+package kr.hhplus.be.server.order.controller;
+
+import kr.hhplus.be.server.common.ApiResponse;
+import kr.hhplus.be.server.order.dto.OrderRequest;
+import kr.hhplus.be.server.order.dto.OrderResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/order")
+public class OrderController {
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<OrderResponse>> createOrder(@RequestBody OrderRequest request) {
+
+        OrderResponse response = new OrderResponse();
+        response.setOrderId("12345");
+        response.setUserId("sampleUserId");
+        response.setTotalPrice(600000);
+        response.setStatus("CREATED");
+        response.setOrderDate("2023-10-01T12:00:00Z");
+
+        ApiResponse<OrderResponse> result = new ApiResponse<>();
+        result.setCode(200);
+        result.setMessage("주문 성공");
+        result.setData(response);
+
+        return new ResponseEntity<>(result,HttpStatus.OK);
+    }
+}

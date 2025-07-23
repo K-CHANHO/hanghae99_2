254f1af3c73dd3a209e21615d16a6a2e369b5d62
K-CHANHO <cksgh1565@gmail.com>
2025-07-23 15:21:01 +0900

    [chore] repository, entity 빈 인식 문제로 경로 직접 설정

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -2,10 +2,12 @@ package kr.hhplus.be.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "kr.hhplus.be.server.balance.repository")
+@EnableJpaRepositories(basePackages = "kr.hhplus.be.server")
+@EntityScan(basePackages = "kr.hhplus.be.server")
 public class ServerApplication {
 
 	public static void main(String[] args) {

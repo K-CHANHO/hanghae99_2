d12b5f58ec165a65d270e36fde17ccee5e5490ef
chanos <cksgh1565@gmail.com>
2025-07-24 23:27:02 +0900

    [test] 쿠폰 발급 통합 테스트

--- src/test/resources/testSql/test-data.sql
+++ src/test/resources/testSql/test-data.sql
@@ -1,4 +1,9 @@
 DELETE FROM balance;
+DELETE FROM coupon;
 
+-- 잔액 샘플 데이터
 INSERT INTO balance (user_id, balance) VALUES ('sampleUserId', 100000);
+
+-- 쿠폰 샘플 데이터
+INSERT INTO coupon (coupon_id, coupon_name, quantity, status, discount_rate) VALUES (1, '깜짝 10% 할인쿠폰', 10, 'AVAILABLE', 0.1);
 

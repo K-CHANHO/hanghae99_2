71d2ac22db8df094f4161f66af5ecd8ccc3f91a8
chanos <cksgh1565@gmail.com>
2025-07-29 22:42:37 +0900

    [refactor] 잔액 내역 구조 변경 및 repo 분리

--- src/main/java/kr/hhplus/be/server/domain/balance/domain/repository/BalanceHistoryRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/balance/domain/repository/BalanceHistoryRepository.java
@@ -1,7 +1,7 @@
 package kr.hhplus.be.server.domain.balance.domain.repository;
 
 import kr.hhplus.be.server.domain.balance.domain.entity.BalanceHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BalanceHistoryRepository extends JpaRepository<BalanceHistory, Long> {
+public interface BalanceHistoryRepository {
+    BalanceHistory save(BalanceHistory balanceHistory);
 }

1fe83bbeb28a253cab56fb8a5039d5c4d2ccb485
K-CHANHO <cksgh1565@gmail.com>
2025-08-11 14:30:55 +0900

    [refactor] 잔액충전 분산락 적용

--- src/main/java/kr/hhplus/be/server/domain/balance/application/service/BalanceService.java
+++ src/main/java/kr/hhplus/be/server/domain/balance/application/service/BalanceService.java
@@ -4,17 +4,26 @@ import kr.hhplus.be.server.domain.balance.application.service.dto.*;
 import kr.hhplus.be.server.domain.balance.domain.entity.Balance;
 import kr.hhplus.be.server.domain.balance.domain.repository.BalanceRepository;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class BalanceService{
 
     private final BalanceRepository balanceRepository;
+    private final RedissonClient redissonClient;
+    private final PlatformTransactionManager transactionManager;
 
     public GetBalanceResult getBalance(GetBalanceCommand getBalanceCommand){
 
@@ -23,20 +32,31 @@ public class BalanceService{
         return GetBalanceResult.from(balance);
     }
 
-    @Retryable(
-            retryFor = {ObjectOptimisticLockingFailureException.class},
-            maxAttempts = 3,
-            backoff = @Backoff(delay = 200)
-    )
-    @Transactional
     public ChargeBalanceResult chargeBalance(ChargeBalanceCommand chargeBalanceCommand) {
-        Balance balance = balanceRepository.findById(chargeBalanceCommand.getUserId())
-                .orElseThrow(() -> new RuntimeException("잔액을 조회할 수 없습니다."));
+        RLock lock = redissonClient.getLock("lock:chargeBalance:" + chargeBalanceCommand.getUserId());
 
-        balance.charge(chargeBalanceCommand.getAmount());
+        try{
+            lock.lock();
+            TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+            try{
+                Balance balance = balanceRepository.findById(chargeBalanceCommand.getUserId())
+                        .orElseThrow(() -> new RuntimeException("잔액을 조회할 수 없습니다."));
+                balance.charge(chargeBalanceCommand.getAmount());
+                Balance chargedBalance = balanceRepository.save(balance);
+                transactionManager.commit(status);
+                return ChargeBalanceResult.from(chargedBalance);
+            } catch (Exception e){
+                transactionManager.rollback(status);
+                throw e;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException();
+        } finally {
+            if(lock.isHeldByCurrentThread()){
+                lock.unlock();
+            }
+        }
 
-        Balance chargedBalance = balanceRepository.save(balance);
-        return ChargeBalanceResult.from(chargedBalance);
     }
 
     @Retryable(

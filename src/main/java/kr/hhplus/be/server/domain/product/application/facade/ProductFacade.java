fb1aa0eb8243b8d420959d740a1c5ab5517dc98c
chanos <cksgh1565@gmail.com>
2025-08-17 17:40:40 +0900

    [feat] 레디스 상태에 따라 분기

--- src/main/java/kr/hhplus/be/server/domain/product/application/facade/ProductFacade.java
+++ src/main/java/kr/hhplus/be/server/domain/product/application/facade/ProductFacade.java
@@ -1,14 +1,15 @@
 package kr.hhplus.be.server.domain.product.application.facade;
 
+import kr.hhplus.be.server.common.RedisHealthChecker;
 import kr.hhplus.be.server.domain.order.application.service.OrderProductService;
 import kr.hhplus.be.server.domain.order.application.service.dto.GetOrderProductsByOrderIdsCommand;
 import kr.hhplus.be.server.domain.order.application.service.dto.GetOrderProductsByOrderIdsResult;
 import kr.hhplus.be.server.domain.payment.application.service.PaymentService;
 import kr.hhplus.be.server.domain.product.application.facade.dto.GetTopProductsResult;
+import kr.hhplus.be.server.domain.product.application.service.ProductRankingService;
+import kr.hhplus.be.server.domain.product.application.service.ProductService;
 import kr.hhplus.be.server.domain.product.application.service.dto.GetProductsCommand;
 import kr.hhplus.be.server.domain.product.application.service.dto.GetProductsResult;
-import kr.hhplus.be.server.domain.product.domain.entity.Product;
-import kr.hhplus.be.server.domain.product.application.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,19 +21,30 @@ public class ProductFacade {
     private final PaymentService paymentService;
     private final OrderProductService orderProductService;
     private final ProductService productService;
+    private final ProductRankingService productRankingService;
+
+    private final RedisHealthChecker redisHealthChecker;
 
     public GetTopProductsResult getTopProducts() {
 
-        // 결제 상태가 PAID이면서 3일 안에 결제된 orderId 조회 (최근 3일)
-        List<Long> orderIds = paymentService.getPaidOrderIdsWithinLastDays(3);
+        GetProductsResult getProductsResult;
 
-        // orderId로 orderProduct 조회하면서 productId로 집계 (top5)
-        GetOrderProductsByOrderIdsCommand getOrderProductsByOrderIdsCommand = GetOrderProductsByOrderIdsCommand.from(orderIds);
-        GetOrderProductsByOrderIdsResult getOrderProductsByOrderIdsResult = orderProductService.getOrderProductsByOrderIds(getOrderProductsByOrderIdsCommand);
+        if(!redisHealthChecker.isRedisAlive()) { // 레디스 서버가 없는 경우 디비 직접 조회
+            // 결제 상태가 PAID이면서 3일 안에 결제된 orderId 조회 (최근 3일)
+            List<Long> orderIds = paymentService.getPaidOrderIdsWithinLastDays(3);
 
-        // productId로 product 조회 (top5)
-        GetProductsCommand getProductsCommand = GetProductsCommand.from(getOrderProductsByOrderIdsResult);
-        GetProductsResult getProductsResult = productService.getProducts(getProductsCommand);
+            // orderId로 orderProduct 조회하면서 productId로 집계 (top5)
+            GetOrderProductsByOrderIdsCommand getOrderProductsByOrderIdsCommand = GetOrderProductsByOrderIdsCommand.from(orderIds);
+            GetOrderProductsByOrderIdsResult getOrderProductsByOrderIdsResult = orderProductService.getOrderProductsByOrderIds(getOrderProductsByOrderIdsCommand);
+
+            // productId로 product 조회 (top5)
+            GetProductsCommand getProductsCommand = GetProductsCommand.from(getOrderProductsByOrderIdsResult);
+            getProductsResult = productService.getProducts(getProductsCommand);
+        } else {
+            List<Long> topProductIds = productRankingService.getTopProductIdsLastNDays(3, 5);
+            GetProductsCommand getProductsCommand = GetProductsCommand.builder().productIds(topProductIds).build();
+            getProductsResult = productService.getProducts(getProductsCommand);
+        }
 
         return GetTopProductsResult.from(getProductsResult);
     }

12dca6926f33daaa95bca2632c97765c8d28c634
chanos <cksgh1565@gmail.com>
2025-08-28 01:20:40 +0900

    [feat] 주문완료 이벤트

--- src/main/java/kr/hhplus/be/server/domain/order/application/event/OrderCompletedEvent.java
+++ src/main/java/kr/hhplus/be/server/domain/order/application/event/OrderCompletedEvent.java
@@ -1,19 +1,19 @@
 package kr.hhplus.be.server.domain.order.application.event;
 
+import kr.hhplus.be.server.domain.order.dto.OrderProductDto;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class OrderCompletedEvent {
     private final Long orderId;
     private final int orderPrice;
+    List<OrderProductDto> orderProductDtoList;
 
-    public OrderCompletedEvent(Long orderId, int orderPrice) {
+    public OrderCompletedEvent(Long orderId, int totalPrice, List<OrderProductDto> orderProductDtoList) {
         this.orderId = orderId;
-        this.orderPrice = orderPrice;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public int getOrderPrice() {
-        return orderPrice;
+        this.orderPrice = totalPrice;
+        this.orderProductDtoList = orderProductDtoList;
     }
 }

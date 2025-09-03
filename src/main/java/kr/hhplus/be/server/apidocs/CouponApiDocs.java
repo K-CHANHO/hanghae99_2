d52bd234f5468f071c1a47a5fba67227fb061e6e
chanos <cksgh1565@gmail.com>
2025-09-04 00:16:14 +0900

    [refactor] kafka 기반 로직으로 변경

--- src/main/java/kr/hhplus/be/server/apidocs/CouponApiDocs.java
+++ src/main/java/kr/hhplus/be/server/apidocs/CouponApiDocs.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.server.apidocs;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -16,7 +17,7 @@ public interface CouponApiDocs {
     @Operation(summary = "쿠폰 발급", description = "사용자에게 선착순으로 쿠폰을 발급합니다.")
     @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "쿠폰 발급 성공")
     @Parameter(name = "transactionId", description = "트랜잭션 ID)", required = true, in = ParameterIn.HEADER)
-    ResponseEntity<ApiResponse<IssueCouponResponse>> issueCoupon(@RequestBody IssueCouponRequest request);
+    ResponseEntity<ApiResponse<IssueCouponResponse>> issueCoupon(@RequestBody IssueCouponRequest request) throws JsonProcessingException;
 
 
 
